aad53ca0c60e90ed8463ca206a89b2b8ba19c9ff
pperi1 <101953389+pperi1@users.noreply.github.com>
2022-04-29 17:30:52 -0700

    Update MyFirstClass.java
    modified MyFirstClass.java

--- hello-world/src/MyFirstPackage/MyFirstClass.java
+++ hello-world/src/MyFirstPackage/MyFirstClass.java
@@ -5,6 +5,7 @@ public class MyFirstClass
 	public static void main(String[] args)
 	{
 		System.out.println("This is my first class");
-		System.out.println("GitHub Integration");
+		
+		System.out.println("GitHub integration successful");
 	}
 }
